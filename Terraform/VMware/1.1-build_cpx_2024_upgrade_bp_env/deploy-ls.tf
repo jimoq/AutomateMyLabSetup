04e70d9e4bf49fa396aa9f6d51109521714c411e
jimoq <jim@oqvist.org>
2024-04-25 09:44:33 +0200

    cleanup

--- Terraform/VMware/1.1-build_cpx_2024_upgrade_bp_env/deploy-ls.tf
+++ Terraform/VMware/1.1-build_cpx_2024_upgrade_bp_env/deploy-ls.tf
@@ -1,6 +1,6 @@
-resource "vsphere_virtual_machine" "cpx-ls" {
+resource "vsphere_virtual_machine" "cpx-ls-test" {
   depends_on           = [ module.vmware ]
-  name                 = "cpx-ls"
+  name                 = "cpx-ls-test"
   datacenter_id        = data.vsphere_datacenter.datacenter.id
   datastore_id         = data.vsphere_datastore.datastore.id
   host_system_id       = data.vsphere_host.host.id
@@ -40,18 +40,18 @@ resource "vsphere_virtual_machine" "cpx-ls" {
     properties = {
       "user_data" = "${base64encode(<<EOF
           #cloud-config
-          hostname: cpx-ls
+          hostname: cpx-ls-test
           password: ${var.chkp_otp_key}
-#          config_system:
-#            domainname: lab.local
-#            timezone: Europe/Stockholm
-#            ftw_sic_key: ${var.chkp_otp_key}
-#            install_security_managment: true
-#            mgmt_admin_radio: gaia_admin
-#            mgmt_gui_clients_radio: any
-#            download_info: true
-#            reboot_if_required : true
-#            maintenance_hash: grub.pbkdf2.sha512.10000.EED96942B7C4CD093DB77FD1C71CFB2DF901F2B8719FFAC15C2C1C8ACF5D8420A8059978CE3DFD5EE057071E1A30E5E34A1D380FA5D212E58E7C50AAFE26F0E0.F0824ED6973BB399CA79F2E743F41137E89DFC73D78B8B0F8D89F6D8F2D78B0AA75AF3939DD554403D32D08047293E263F11541B0816032754A9DB9784253509
+          config_system:
+            domainname: lab.local
+            timezone: Europe/Stockholm
+            ftw_sic_key: ${var.chkp_otp_key}
+            install_security_managment: true
+            mgmt_admin_radio: gaia_admin
+            mgmt_gui_clients_radio: any
+            download_info: true
+            reboot_if_required : true
+            maintenance_hash: grub.pbkdf2.sha512.10000.EED96942B7C4CD093DB77FD1C71CFB2DF901F2B8719FFAC15C2C1C8ACF5D8420A8059978CE3DFD5EE057071E1A30E5E34A1D380FA5D212E58E7C50AAFE26F0E0.F0824ED6973BB399CA79F2E743F41137E89DFC73D78B8B0F8D89F6D8F2D78B0AA75AF3939DD554403D32D08047293E263F11541B0816032754A9DB9784253509
 #          runcmd:
 #            - chmod 0777 /var/log/tmp
 #            - rm -rf /var/tmp/*;rmdir /var/tmp;ln -s /var/log/tmp /var/tmp
@@ -67,7 +67,7 @@ resource "vsphere_virtual_machine" "cpx-ls" {
               name: eth0
               subnets:
                 - type: static
-                  address: 192.168.233.43/24
+                  address: 192.168.233.44/24
                   gateway: 192.168.233.254
                   dns_nameservers: [192.168.233.233, 8.8.8.8]
       EOF
